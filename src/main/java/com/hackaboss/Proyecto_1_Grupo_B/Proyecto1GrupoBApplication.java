de3716e2ec2ca6e208e42a88ef15059d5836b085
jacerc <javiercerveracentenero@gmail.com>
2026-04-03 11:30:04 +0200

    Primer Commit

---
+++ src/main/java/com/hackaboss/Proyecto_1_Grupo_B/Proyecto1GrupoBApplication.java
@@ -0,0 +1,13 @@
+package com.hackaboss.Proyecto_1_Grupo_B;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Proyecto1GrupoBApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Proyecto1GrupoBApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hackaboss/Proyecto_1_Grupo_B/Proyecto1GrupoBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hackaboss.Proyecto_1_Grupo_B;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Proyecto1GrupoBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
